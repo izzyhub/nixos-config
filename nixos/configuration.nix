ab28bce66c6cb99a02c7ab8ecccedd9329ff90e2
Izzy <izzy@izzywatson.com>
2023-12-30 05:57:54 +0000

    Fix networkmanager not being installed.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -72,6 +72,7 @@
   };
 
   networking.hostName = "nixpad";
+  networking.networkmangager.enable = true;
 
   # TODO: This is just an example, be sure to use whatever bootloader you prefer
   boot.loader.systemd-boot.enable = true;
@@ -89,7 +90,7 @@
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
       ];
       # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-      extraGroups = ["wheel"];
+      extraGroups = ["wheel" "networkmanager" "audio" "docker"];
     };
   };
 
